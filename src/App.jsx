de6c322dd08ad55257864ecc1f5cdf68510815f7
AuroraFelix24 <133307178+AuroraFelix24@users.noreply.github.com>
2023-10-03 23:59:41 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [updateInfo, setUpdateInfo] = useState()
 
 
-const baseUrl= 'https://users-crud.academlo.tech/'
+const baseUrl= 'https://userscrud-0nlt.onrender.com'
 
 const [
   users, 
